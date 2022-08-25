34bada34d407f1840fe6b8df803cfcbb664f2f3b
hyelim3 <shl0703ss@gmail.com>
2022-08-25 23:49:29 +0900

    멤버수정 및 수정

--- app.js
+++ app.js
@@ -15,11 +15,63 @@ const pool = mysql.createPool({
 const app = express();
 const port = 4000;
 app.use(cors());
+app.use(express.json());
 
 app.get("/member", async (req, res) => {
   const [rows] = await pool.query("SELECT * FROM member");
 
   res.json(rows);
+});
+//포스트맨 db수정
+app.patch("/members/:id", async (req, res) => {
+  const { id } = req.params;
+  const { 거주지, 연락처 } = req.body;
+
+  const [rows] = await pool.query(
+    `
+    select *
+    from member
+    where id =?
+    `,
+    [id]
+  );
+
+  if (rows.length === 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+  }
+
+  if (!거주지) {
+    res.status(400).json({
+      msg: "거주지 required",
+    });
+  }
+
+  if (!연락처) {
+    res.status(400).json({
+      msg: "연락처 required",
+    });
+  }
+
+  const [rs] = await pool.query(
+    `
+    update member
+    set 거주지 = ?,
+    연락처 = ?
+    where id = ?
+    `,
+    [거주지, 연락처, id]
+  );
+
+  const [updatedMembers] = await pool.query(
+    `
+    select *
+    from member
+    order by id asc
+    `
+  );
+  res.json(updatedMembers);
 });
 
 //db수정
@@ -54,6 +106,43 @@ app.patch("/members/check/:id", async (req, res) => {
     from member
     order by id ASC
     `
+  );
+  res.json(updatedMembers);
+});
+//삭제 db연동
+app.delete("/members/check/:id", async (req, res) => {
+  const { id } = req.params;
+  const [[memberRow]] = await pool.query(
+    `
+    select *
+    from member
+    where id =?
+    `,
+    [id]
+  );
+
+  if (memberRow === undefined) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  await pool.query(
+    `
+    delete
+    from member
+    where id = ?
+    `,
+    [id]
+  );
+
+  const [updatedMembers] = await pool.query(
+    `
+    select *
+    from member
+    order by id asc
+     `
   );
   res.json(updatedMembers);
 });
